99eb2fc2080cf9815966b90d8a30554b6ea42da3
pop1234o <859405648@qq.com>
2019-08-07 10:35:33 +0800

    feat:添加了知识

---
+++ app/src/main/java/com/liyafeng/kotlinproject/practice/Question.kt
@@ -0,0 +1,17 @@
+package com.liyafeng.kotlinproject.practice
+
+class Question {
+
+
+    /**
+     * Unresolved reference: base
+     * 组件化依赖别的lib的的父类时，报错，好像找不到这个类。。
+     * 结果是lib的build.gradle中缺少kotlin的编译配置
+     * apply plugin: 'kotlin-android'
+     * apply plugin: 'kotlin-android-extensions'
+     *
+     * */
+    fun a1(): Unit {
+
+    }
+}
