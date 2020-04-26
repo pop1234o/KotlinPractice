f962c0ba21de9bcbcf3fd9173fb48e1821b865bc
pop <859405648@qq.com>
2020-04-26 10:35:53 +0800

    refactor:升级了版本

--- app/src/main/java/com/liyafeng/kotlinproject/MainActivity.kt
+++ app/src/main/java/com/liyafeng/kotlinproject/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.liyafeng.kotlinproject
 
+//import com.tinkerpatch.sdk.TinkerPatch
+//import com.tinkerpatch.sdk.server.callback.ConfigRequestCallback
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import com.tinkerpatch.sdk.TinkerPatch
-import com.tinkerpatch.sdk.server.callback.ConfigRequestCallback
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -24,19 +23,19 @@ class MainActivity : AppCompatActivity() {
 
         tv_cotent.text = "dddd"
 
-        button1.setOnClickListener { TinkerPatch.with().fetchPatchUpdate(true); }
-        button2.setOnClickListener {
-            TinkerPatch.with().fetchDynamicConfig(object : ConfigRequestCallback {
-
-                override fun onSuccess(configs: HashMap<String, String>) {
-                    Log.w("test", "request config success, config:$configs")
-                }
-
-                override fun onFail(e: Exception) {
-                    Log.w("test", "request config failed, exception:$e")
-                }
-            }, true)
-        }
+//        button1.setOnClickListener { TinkerPatch.with().fetchPatchUpdate(true); }
+//        button2.setOnClickListener {
+//            TinkerPatch.with().fetchDynamicConfig(object : ConfigRequestCallback {
+//
+//                override fun onSuccess(configs: HashMap<String, String>) {
+//                    Log.w("test", "request config success, config:$configs")
+//                }
+//
+//                override fun onFail(e: Exception) {
+//                    Log.w("test", "request config failed, exception:$e")
+//                }
+//            }, true)
+//        }
 
     }
 }
